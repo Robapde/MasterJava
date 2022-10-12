d8d253cd893a9f6bd68c83207c35b23a55d8b38b
= <=>
2022-10-12 20:58:01 +0200

    End java basic

--- JavaMaster/TypeDatas/If_then_Statement/src/Main.java
+++ JavaMaster/TypeDatas/If_then_Statement/src/Main.java
@@ -2,8 +2,25 @@ public class Main {
     public static void main(String[] args) {
 
         boolean isAlien = false;
-        if ( isAlien == false )
-            System.out.println("It is not an alien!");
-        
+        if ( !isAlien )  // ! == not
+            System.out.println("It is  an alien!");
+
+
+
+        int topScore = 20;
+        if ( topScore > 30 && topScore < 100 ){ //&& == and
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        int secondTopScore = 120;
+        if ( (topScore > 30 && topScore < 100) || secondTopScore > 100 ){ // || == or
+            System.out.println("Greater than second top score and less than 100 || greater ");
+        }
+
+
+        // Ternary Operator
+        boolean isCar = false;
+
+        boolean wasCar = !isCar  ? true : false;
     }
 }
