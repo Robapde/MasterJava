ac608054df18abfe2310123d128acfdbc4126198
= <=>
2022-10-12 19:55:56 +0200

    Sube ya

--- JavaMaster/TypeDatas/If_then_Statement/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        boolean isAlien = false;
-        if ( isAlien == false )
-            System.out.println("It is not an alien!");
-        
-    }
-}
